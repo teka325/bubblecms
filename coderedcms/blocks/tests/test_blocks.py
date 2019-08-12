556d1c1d98d618bfd4c4c90b0ab1ff3aac5b287a
Kevin <kevincummings3@outlook.com>
2019-08-12 15:33:22 -0400

    Saving work

--- coderedcms/blocks/tests/test_blocks.py
+++ coderedcms/blocks/tests/test_blocks.py
@@ -1,7 +1,9 @@
-from coderedcms.blocks import base_blocks
+from coderedcms.blocks import base_blocks, html_blocks
 from django.test import SimpleTestCase
 
 from wagtail.tests.utils import WagtailTestUtils
+
+from coderedcms.settings import cr_settings
 
 
 class TestMultiSelectBlock(WagtailTestUtils, SimpleTestCase):
@@ -19,3 +21,18 @@ class TestMultiSelectBlock(WagtailTestUtils, SimpleTestCase):
         self.assertInHTML('<option value="tea" selected>Tea</option>', html)
         self.assertInHTML('<option value="coffee" selected>Coffee</option>', html)
         self.assertTrue(html.count('selected'), 2)
+
+
+# class TestHeaderBlocks(WagtailTestUtils, SimpleTestCase):
+#     def test_h1_block(self):
+#         block = html_blocks.H1Block(text='Test Header')
+#         html = block.render_form('Test Header')
+
+
+class TestButtonBlock(WagtailTestUtils, SimpleTestCase):
+    def test_render_button_block(self):
+        block = html_blocks.ButtonBlock(other_link='https://coderedcorp.com', button_title='Go To Codered')
+        value = {'settings': {'custom_template': block.child_blocks['settings'].child_blocks['custom_template']._constructor_args[1]['choices'][0][0]}}
+        html = block.render(value)
+        print(html)
+        self.assertInHTML('<a href="{}"'.format('https://coderedcorp.com'), html)
